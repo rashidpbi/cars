d0ae88ef6918235d1ecfdeb66ac03e4e2fe9a131
rashidpbi <rashidpbi786@gmail.com>
2024-02-22 17:15:32 +0530

    bold added when term matches in already existing

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -3,8 +3,12 @@ import { useSelector, useDispatch } from 'react-redux'
 import { removeCar } from '../store';
 function CarList() {
   const dispatch = useDispatch();
-  const cars = useSelector(({cars:{data,searchTerm}})=>{
-    return data.filter((car)=>car.name.toLowerCase().includes(searchTerm.toLowerCase()))
+  const {cars,name} = useSelector(({form,cars:{data,searchTerm}})=>{
+    const filteredCars=  data.filter((car)=>car.name.toLowerCase().includes(searchTerm.toLowerCase()))
+    return{
+      cars:filteredCars,
+      name:form.name
+    }
   })
 
   const handleCarDelete =(car)=>{
@@ -12,8 +16,9 @@ function CarList() {
   }
 
   const renderedCars = cars.map((car)=>{
+    const bold = name && car.name.toLowerCase().includes(name.toLowerCase())
     return(
-      <div key={car.id} className='panel'>
+      <div key={car.id} className={`panel ${bold && 'bold' }`}>
         <p>
           {car.name} - ${car.cost}
         </p>
