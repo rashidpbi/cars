7b865c94a2c995e26262f02b412f512d602842b2
rashidpbi <rashidpbi786@gmail.com>
2024-02-22 16:37:53 +0530

    simple sum logic in carvalue.js

--- src/components/CarValue.js
+++ src/components/CarValue.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import { UseSelector, useSelector } from "react-redux"
+
 
 function CarValue() {
+  const totalCost = useSelector(({cars:{data,searchTerm}})=>{
+    const filteredCars = data.filter((car)=>{
+      return car.name.toLowerCase().includes(searchTerm.toLowerCase());
+    });
+    let cost = 0;
+    for (let car of filteredCars){
+      cost += car.cost
+    }
+    return cost;
+
+  })
   return (
-    <div>
-      CarValue
+    <div className='car-value'>
+      Total cost : ${totalCost}
     </div>
   )
 }
